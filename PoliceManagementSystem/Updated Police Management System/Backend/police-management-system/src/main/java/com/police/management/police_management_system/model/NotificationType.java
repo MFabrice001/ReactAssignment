5a30bf3096c64a8a669eb2f02610ea27e2fbefe8
MFabrice001 <m.fabrice001@gmail.com>
2024-12-01 21:40:20 +0200

    update

--- PoliceManagementSystem/Updated Police Management System/Backend/police-management-system/src/main/java/com/police/management/police_management_system/model/NotificationType.java
+++
@@ -1,6 +0,0 @@
-package com.police.management.police_management_system.model;
-
-public enum NotificationType {
-    CRIME_REPORT_UPDATE,
-    GENERAL_ALERT
-}
--- PoliceManagementSystem/Updated Police Management System/Backend/police-management-system/src/main/java/com/police/management/police_management_system/model/Role.java
+++
@@ -1,8 +0,0 @@
-package com.police.management.police_management_system.model;
-
-public enum Role {
-    ROLE_ADMIN,
-    ROLE_STAFF,
-    ROLE_CITIZEN,
-    //ROLE_POLICE_STAFF
-}
